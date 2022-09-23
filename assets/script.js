c7fc325fff5de27791a10ec3c63160a5ed79b973
james <james.anthony.russell36@gmail.com>
2022-09-23 10:36:56 +1000

    creating html elements through jquery

--- assets/script.js
+++ assets/script.js
@@ -3,7 +3,7 @@
 let city = $("#searchTerm").val()
 
 // const variable to store api key
-const apiKey = "&appid=";
+const apiKey = "&appid=419a6f585c25a4bb328b301f9ff349f4";
 // date variable using Date to turn date/time into a string.
 let date = new Date();
 
@@ -16,8 +16,62 @@ $("#searchTerm").keypress(function(event) {
 });
 
 $("#searchTerm").on("click", function() {
-
+    //use css to show the forcast header
     $('#forcastH5').addClass('show');
 
-    city = $("searchTerm").val();
-})
+    // get value from the input tag store in city
+    city = $("#searchTerm").val();
+
+    //clear the input box
+    $("#searchTerm").val("");
+
+    //api url with city input and api key
+
+    const queryUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + apiKey;
+    
+    // url
+    // Type: String
+    // A string containing the URL to which the request is sent.
+    // settings
+    // Type: PlainObject
+    // A set of key/value pairs that configure the Ajax request. 
+    // All settings are optional. 
+    // A default can be set for any option with $.ajaxSetup(). 
+    // See jQuery.ajax( settings ) below for a complete list of all settings.
+    // use the ajax me
+
+    $.ajax({
+        url: queryUrl,
+        method:"GET"
+    })
+    .then(function (response){
+
+        console.log(response)
+        console.log(response.name)
+
+    })
+
+    
+
+});
+
+// get the current conditions for the card
+function getCurrentConditions() {
+    //start with empty tag
+    $("#currentCity").empty()
+
+    // get set data
+    // pull the class styling straight from bootstrap.. interesting
+    const card = $("<div>").addClass("Card");
+    const cardbody =
+    const city
+    const cityDate
+    const image
+    const temperature
+    const humidity
+    const wind
+    const 
+}
+
+
+
